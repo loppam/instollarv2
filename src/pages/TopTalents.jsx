574d582a4c7ea1d6d3cf2b6acde831161e4312ab
loppam <lolade132@gmail.com>
2023-12-20 17:24:07 +0100

    Instollar

--- src/pages/TopTalents.jsx
+++ src/pages/TopTalents.jsx
@@ -1,130 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import { MdLocationOn } from "react-icons/md";
-// import Navbar from "../components/Navbar";
-// import MainFooter from "../components/MainFooter";
-// import Footer from "../components/footer";
-// import { Link } from "react-router-dom";
-// const TopTalents = () => {
-//   const [data, setData] = useState(null);
-//   const [loading, setLoading] = useState(true);
-//   const [error, setError] = useState(null);
-//   const apiUrl = "https://randomuser.me/api/?results=13";
-//   const timeout = 5000;
-
-//   function fetchDataWithTimeout() {
-//     const timeoutPromise = new Promise((_, reject) => {
-//       setTimeout(() => {
-//         reject(new Error("Timeout error: The request took too long."));
-//       }, timeout);
-//     });
-
-//     const fetchDataPromise = fetch(apiUrl).then((response) => {
-//       if (!response.ok) {
-//         throw new Error(`HTTP error! Status: ${response.status}`);
-//       }
-//       return response.json();
-//     });
-
-//     return Promise.race([fetchDataPromise, timeoutPromise]);
-//   }
-//   // useEffect(() => {
-//   //   const userFetch = 13;
-//   //   fetch(`https://randomuser.me/api/?results=${userFetch}`)
-//   //     .then((response) => {
-//   //       if (!response.ok) {
-//   //         throw new Error("Network response was not ok");
-//   //       }
-//   //       return response.json();
-//   //     })
-//   //     .then((userData) => {
-//   //       setData(userData.results);
-//   //       setLoading(false);
-//   //     })
-//   //     .catch((error) => {
-//   //       setError(error);
-//   //       setLoading(false);
-//   //     });
-//   // }, []);
-//   console.log(data);
-//   if (loading) {
-//     return (
-//       <div className="loader-container">
-//         <div className="lds-dual-ring"></div>
-//       </div>
-//     );
-//   }
-
-//   if (error) {
-//     return <p className="ferror">Error: {error.message}</p>;
-//   }
-//   return (
-//     <div>
-//       <Navbar />
-//       <div className="top_talent_head">
-//         <h2>Green Energy Talents</h2>
-//         <p>Browse for talents. Filter your search below</p>
-//         <div className="topthflex">
-//           <p>Filtered By:</p>
-//           {/* {data.length > 0 && (
-//             <select className="">
-//               <option value="">All Locations (states)</option>
-//               {data.map((user, index) => (
-//                 <option key={index} value="">
-//                   {user.location.state}
-//                 </option>
-//               ))}
-//             </select>
-//           )}
-//           {data.length > 0 && (
-//             <select className="">
-//               <option value="">Years of Experience</option>
-//               {data.map((user, index) => (
-//                 <option key={index} value="">
-//                   {user.registered.age}
-//                 </option>
-//               ))}
-//             </select>
-//           )} */}
-//           <select name="" id="">
-//             <option value="">Badge</option>
-//             <option value="">Sunking</option>
-//             <option value="">Koolboks</option>
-//           </select>
-//         </div>
-//       </div>
-//       <div className="top-talent">
-//         {data.length > 0 && (
-//           <div className="flex_container">
-//             {data.map((user) => (
-//               <div className="card" key={user.login.uuid}>
-//                 <Link className="card" to={`/user/${user.login.uuid}`}>
-//                   <div className="center">
-//                     <img
-//                       src={user.picture.large}
-//                       alt={`User ${user.name.first}`}
-//                     />
-//                   </div>
-//                   <h3>
-//                     {user.name.first} {user.name.last}
-//                   </h3>
-//                   <p>
-//                     <MdLocationOn /> {user.location.state} {", "}{" "}
-//                     {user.location.country}
-//                   </p>
-//                 </Link>
-//               </div>
-//             ))}
-//           </div>
-//         )}
-//       </div>
-//       <Footer />
-//       <MainFooter />
-//     </div>
-//   );
-// };
-
-// export default TopTalents;
-
 import React, { useState, useEffect } from "react";
 import { MdLocationOn } from "react-icons/md";
 import Navbar from "../components/Navbar";
@@ -137,7 +10,7 @@ const TopTalents = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const apiUrl = "https://randomuser.me/api/?results=15";
-  const timeout = 15000;
+  const timeout = 5000;
 
   function fetchDataWithTimeout() {
     const timeoutPromise = new Promise((_, reject) => {
@@ -155,7 +28,7 @@ const TopTalents = () => {
 
     return Promise.race([fetchDataPromise, timeoutPromise]);
   }
-
+  console.log(data);
   useEffect(() => {
     fetchDataWithTimeout()
       .then((userData) => {
@@ -167,8 +40,6 @@ const TopTalents = () => {
         setLoading(false);
       });
   }, []); // Empty dependency array ensures the effect runs only once on component mount
-
-  console.log(data);
 
   if (loading) {
     return (
@@ -222,7 +93,11 @@ const TopTalents = () => {
           <div className="flex_container">
             {data.map((user) => (
               <div className="card" key={user.login.uuid}>
-                <Link className="card" to={`/user/${user.login.uuid}`}>
+                <Link
+                  className="card"
+                  to={`/user/${user.login.uuid}`}
+                  onClick={() => console.log(user.login.uuid)}
+                >
                   <div className="center">
                     <img
                       src={user.picture.large}
--- src/pages/TopTalentsDetails.jsx
+++ src/pages/TopTalentsDetails.jsx
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { MdLocationOn } from "react-icons/md";
+import Navbar from "../components/Navbar";
+import Footer from "../components/footer";
+import MainFooter from "../components/MainFooter";
+
 const TopTalentsDetails = () => {
-  const { uuid } = useParams;
+  const { uuid } = useParams();
   const [userDetails, setUserDetails] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
+  console.log("UUID:", uuid);
   useEffect(() => {
     fetch(`https://randomuser.me/api/?seed=${uuid}`)
       .then((response) => {
@@ -23,17 +28,76 @@ const TopTalentsDetails = () => {
         setLoading(false);
       });
   }, [uuid]);
+
+  if (loading) {
+    return (
+      <div className="loader-container">
+        <div className="lds-dual-ring"></div>
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
     <div>
       <div className="">
         {userDetails && (
           <div className="">
-            <img src={userDetails.picture.large} alt="" />
-            <p>Name: {`${userDetails.name.first} ${userDetails.name.last}`}</p>
-            <p>
-              <MdLocationOn /> {user.location.state} {", "}{" "}
-              {user.location.country}
-            </p>
+            <Navbar />
+            <div className="border">
+              <div className="border_top">
+                <img src={userDetails.picture.large} alt="" />
+                <div className="border_top_txt">
+                  <h1>
+                    {`${userDetails.name.first} ${userDetails.name.last}`}
+                  </h1>
+                  <p>
+                    <MdLocationOn /> {userDetails.location.state} {", "}{" "}
+                    {userDetails.location.country}
+                  </p>
+                  <button className="border_btn">
+                    Please Login to request a quote
+                  </button>
+                </div>
+              </div>
+              <div className="flex_border">
+                <div className="border_left">
+                  <div className="overview">
+                    <h1>Overview</h1>
+                    <p>Summary</p>
+                  </div>
+                  <hr />
+                  <div className="certifications">
+                    <h2>Certifications</h2>
+                    <p>No Certifications Found!</p>
+                    <h2>Languages</h2>
+                    <p>English</p>
+                    <p>Yoruba</p>
+                  </div>
+                  <hr />
+                  <div className="brands">
+                    <h2>Familiar Brands</h2>
+                    <p>Huwaei</p>
+                    <p>Luminus</p>
+                  </div>
+                  <hr />
+                </div>
+                <hr />
+                <div className="border_right">
+                  <h2>Work Gallery</h2>
+                  <div className="img">
+                    <img src="/toptalent/work.jpg" alt="" />
+                    <img src="/toptalent/work.jpg" alt="" />
+                    <img src="/toptalent/work.jpg" alt="" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <Footer />
+            <MainFooter />
           </div>
         )}
       </div>
